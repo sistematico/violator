f452a1d6a318955ba0247aae72023db702d97279
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-05-30 17:50:25 -0400

    Update

---
+++ config/blacklist.py
@@ -0,0 +1,19 @@
+blacklist = [
+    'chat.whatsapp',
+    'wa.me',
+    't.me',
+    'api.whatsapp',
+    'kwai.app',
+    'nos uma visita',
+    'financiamento negado',
+    'veículos novos e semi',
+    'faça seu orçamento',
+    'veículos semi-novo',
+    'plano sem juro',
+    'planos sem juro',
+    'serviços de frete',
+    'adestramento inteligente',
+    'ensine seu cão',
+    'ensine seu cachorro',
+    'raissa sampaio'
+]
