d5a1596c630f8fd9ed1de30352cc0bc1a402d62c
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-06-03 12:39:37 -0400

    Update automático

--- violator/warn.py
+++ violator/warn.py
@@ -30,5 +30,6 @@ def warn(update: Update, context: CallbackContext) -> str:
     
     
     # update.message.reply_text(key)
-    update.message.reply_text(repr(update))
+    # update.message.reply_text(repr(update))
+    update.message.reply_text(print vars(update))
 
