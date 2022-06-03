e5e4fca79a1288ae39541d04728954f4dc4b54af
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-06-03 10:00:16 -0400

    Update automático

--- violator/decorators.py
+++ violator/decorators.py
@@ -1,13 +1,5 @@
 from functools import wraps
 
-# def restricted(func):
-#     @wraps(func)
-#     async def wrapped(update, context):
-#         if update.effective_user.id in update.get_chat_administrators(update.effective_chat.id):
-#             context.bot.send_message(update.message.chat_id, text="É admin")
-#             return
-#         return await func(update, context)
-#     return wrapped
 def restricted(func):
     @wraps(func)
     def wrapped(update, context):
